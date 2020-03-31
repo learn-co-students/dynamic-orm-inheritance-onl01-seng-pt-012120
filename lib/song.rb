38a48e0059a7ab226a2450c928dfab79a1c3ae7f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-31 08:24:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 require_relative "./interactive_record.rb"
 
-class Song < InteractiveRecord
+class Song < InteractiveRecord #inheritance
 
+#here column_names is inherited from super class
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
