4ae6b093ac371edcbb0e23e21b40decb24db1b80
Rico Mitchell <ricomitch@icloud.com>
2020-04-07 04:02:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
-require_relative "./interactive_record.rb"
+class Song < InteractiveRecord #inheritance
 
-class Song < InteractiveRecord
-
+#here column_names is inherited from super class
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
